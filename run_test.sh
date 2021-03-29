4168c04338534ad5ab39bae6cc74ea55dddaca79
daodaofr <yanyichao91@gmail.com>
2021-03-29 10:00:29 +0400

    Update run_test.sh

--- run_test.sh
+++ run_test.sh
@@ -1,12 +1,13 @@
 
 # CUHK-SUSU AlignPS
 TESTPATH='fcos_center-normbbox-centeronreg-giou_r50_caffe_fpn_gn-head_dcn_4x4_1x_cuhk_reid_1500_stage1_fpncat_dcn_epoch24_multiscale_focal_x4_bg-2_lconv3dcn_sub_triqueue_dcn0'
+TESTNAME='cuhk_alignps.pth'
 
 # CUHK-SUSU AlignPS+
 # TESTPATH='fcos_center-normbbox-centeronreg-giou_r50_caffe_fpn_gn-head_dcn_4x4_1x_cuhk_reid_1500_stage1_fpncat_dcn_epoch24_multiscale_focal_x4_bg-2_lconv3dcn_sub_triqueue_dcn0'
+# TESTNAME='cuhk_alignps_plus.pth'
 
-i=24
-./tools/dist_test.sh ./configs/fcos/${TESTPATH}.py work_dirs/${TESTPATH}/epoch_${i}.pth 1 --out work_dirs/${TESTPATH}/results_1000.pkl
+./tools/dist_test.sh ./configs/fcos/${TESTPATH}.py work_dirs/${TESTPATH}/${TESTNAME} 1 --out work_dirs/${TESTPATH}/results_1000.pkl
 echo '------------------------'
 python ./tools/test_results.py ${TESTPATH}
 echo $TESTPATH
