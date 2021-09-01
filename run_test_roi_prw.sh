f130f54d8bc5a32f5ec1083fb042bd13bdcfce45
daodaofr <yanyichao91@gmail.com>
2021-09-01 17:05:43 +0800

    Create run_test_roi_prw.sh

---
+++ run_test_roi_prw.sh
@@ -0,0 +1,8 @@
+# CUHK-SUSU AlignPS
+TESTPATH='faster_rcnn_r50_caffe_c4_1x_cuhk_single_two_stage176_nae1_prw'
+TESTNAME='prw_roi_alignps.pth'
+
+# Make sure the model path is work_dirs/TESTPATH/TESTNAME, tesults_1000.pkl will be saved in work_dirs/TESTPATH
+./tools/dist_test_d.sh ./configs/person_search_prw/${TESTPATH}.py work_dirs/${TESTPATH}/epoch_${i}.pth 1 --out work_dirs/${TESTPATH}/results_1000.pkl
+echo '------------------------'
+python ./tools/test_results_psd_prw.py ${TESTPATH}
