e255e53e9a5ec8036c95df6ee23a074a43b393be
daodaofr <yanyichao91@gmail.com>
2021-09-01 16:52:15 +0800

    Update __init__.py

--- mmdet/models/roi_heads/bbox_heads/__init__.py
+++ mmdet/models/roi_heads/bbox_heads/__init__.py
@@ -4,6 +4,7 @@ from .convfc_bbox_head import (ConvFCBBoxHead, Shared2FCBBoxHead,
 from .double_bbox_head import DoubleConvFCBBoxHead
 from .sabl_head import SABLHead
 
+from .bbox_head_bn import BBoxHeadBN
 from .person_search_bbox_head_nae_newoim_2input_bn import PersonSearchNormAwareNewoim2InputBNBBoxHead
 from .person_search_bbox_head_nae_newoim_2input_bn_prw import PersonSearchNormAwareNewoim2InputBNBBoxHeadPRW
 
