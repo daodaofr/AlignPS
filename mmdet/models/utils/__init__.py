94473a5f7e9c965ba3c65120e89d07062e924e1a
daodaofr <yanyichao91@gmail.com>
2021-09-01 16:43:55 +0800

    Update __init__.py

--- mmdet/models/utils/__init__.py
+++ mmdet/models/utils/__init__.py
@@ -1,4 +1,5 @@
 from .gaussian_target import gaussian_radius, gen_gaussian_target
 from .res_layer import ResLayer
+from .mi_estimators import MINE
 
-__all__ = ['ResLayer', 'gaussian_radius', 'gen_gaussian_target']
+__all__ = ['ResLayer', 'gaussian_radius', 'gen_gaussian_target', 'MINE']
