36233ce796aaad19772a99853e5b1798ec184934
daodaofr <yanyichao91@gmail.com>
2021-09-01 16:14:56 +0800

    Update __init__.py

--- mmdet/models/detectors/__init__.py
+++ mmdet/models/detectors/__init__.py
@@ -26,11 +26,12 @@ from .two_stage import TwoStageDetector
 from .yolo import YOLOV3
 from .single_stage_reid import SingleStageReidDetector
 from .single_stage_reid_fpn import SingleStageReidFpnDetector
+from .single_two_stage17_6_prw import SingleTwoStageDetector176PRW
 
 __all__ = [
     'ATSS', 'BaseDetector', 'SingleStageDetector', 'TwoStageDetector', 'RPN',
     'FastRCNN', 'FasterRCNN', 'MaskRCNN', 'CascadeRCNN', 'HybridTaskCascade',
     'RetinaNet', 'FCOS', 'FCOSReid', 'FCOSReidFpn', 'GridRCNN', 'MaskScoringRCNN', 'RepPointsDetector',
     'RepPointsDetectorReid', 'FOVEA', 'FSAF', 'NASFCOS', 'PointRend', 'GFL', 'CornerNet', 'PAA',
-    'YOLOV3', 'SingleStageReidDetector', 'SingleStageReidFpnDetector'
+    'YOLOV3', 'SingleStageReidDetector', 'SingleStageReidFpnDetector', 'SingleTwoStageDetector176PRW'
 ]
